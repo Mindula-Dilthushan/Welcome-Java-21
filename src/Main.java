8969ec512c91f659c170efe526b3c711605373d3
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-06-14 08:05:58 +0530

    update-readme

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * project name    : welcome java 21
+ * project author  : mindula dilthushan
+ * author email    : minduladilthushan1@gmail.com
+ *
+ */
+
 void main(){
     System.out.println("Hello World!");
 }
